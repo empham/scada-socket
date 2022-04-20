dbb7f06abd37c96141fc3ce03543cbae128d7c31
Emily Pham <emilyn3@hawaii.edu>
2022-04-19 17:10:39 -1000

    changed to IP address
    changed sockets to use hardcoded IP addr instead of INADDR_ANY

--- socket.c
+++ socket.c
@@ -27,13 +27,15 @@ int createSocket( int* sockfd, struct sockaddr_in* sockAddress, int port, int ty
       // specify address for socket
       sockAddress->sin_family = AF_INET;
       sockAddress->sin_port = htons(port);
-      sockAddress->sin_addr.s_addr = INADDR_ANY;
+      //sockAddress->sin_addr.s_addr = INADDR_ANY;
+      sockAddress->sin_addr.s_addr = inet_addr("10.1.1.8");
    }
    else if (type == SERVER) {
       // specify address for socket
       sockAddress->sin_family = AF_INET;
       sockAddress->sin_port = htons(port);
-      sockAddress->sin_addr.s_addr = INADDR_ANY;
+      //sockAddress->sin_addr.s_addr = INADDR_ANY;
+      sockAddress->sin_addr.s_addr = inet_addr("10.1.1.170");
 
       //bind the socket to specified IP{ and port
       bind(*sockfd, (struct sockaddr*) sockAddress, sizeof(*sockAddress));
